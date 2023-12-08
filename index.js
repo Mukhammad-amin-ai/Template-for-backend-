5ce926b104acbfcdafc5c6b0fdc9fee8f515d9d1
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2023-12-08 19:59:51 +0500

    seagger

--- index.js
+++ index.js
@@ -1,14 +1,57 @@
 import Express from "express";
 import mongoose from "mongoose";
 import router from "./routes/router.js";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from 'swagger-ui-express'
+
+
+
+
 
 const app = Express();
 const PORT = 3000;
 const DB = 'mongodb://localhost:27017/todo';
+const options = {
+    definition: {
+        openapi: "3.1.0",
+        info: {
+            title: "LogRocket Express API with Swagger",
+            version: "0.1.0",
+            description:
+                "This is a simple CRUD API application made with Express and documented with Swagger",
+            license: {
+                name: "MIT",
+                url: "https://spdx.org/licenses/MIT.html",
+            },
+            contact: {
+                name: "LogRocket",
+                url: "https://logrocket.com",
+                email: "info@email.com",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+        ],
+    },
+    apis: ["./routes/router.js"],
+};
+
+const specs = swaggerJSDoc(options);
+
+
 
 
 app.use(Express.json());
 app.use('/api', router);
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs, { explorer: true })
+);
+
+
 
 const StarterFunc = async () => {
     try {
--- routes/router.js
+++ routes/router.js
@@ -5,6 +5,72 @@ import todo from "../controllers/todo/todo.js";
 let router = new Router()
 
 // Authentification
+// /**
+//  * @swagger
+//  * components:
+//  *   schemas:
+//  *     Book:
+//  *       type: object
+//  *       required:
+//  *         - title
+//  *         - author
+//  *         - finished
+//  *       properties:
+//  *         id:
+//  *           type: string
+//  *           description: The auto-generated id of the book
+//  *         title:
+//  *           type: string
+//  *           description: The title of your book
+//  *         author:
+//  *           type: string
+//  *           description: The book author
+//  *         finished:
+//  *           type: boolean
+//  *           description: Whether you have finished reading the book
+//  *         createdAt:
+//  *           type: string
+//  *           format: date
+//  *           description: The date the book was added
+//  *       example:
+//  *         id: d5fE_asz
+//  *         title: The New Turing Omnibus
+//  *         author: Alexander K. Dewdney
+//  *         finished: false
+//  *         createdAt: 2020-03-10T04:05:06.157Z
+//  */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: Authentification to  managing API
+ * /api/register:
+ *   post:
+ *     summary: Create a new todo
+ *     tags: [register]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: 'api/register'
+ *     responses:
+ *       200:
+ *         description: The created book.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: 'api/register'
+ *       500:
+ *         description: Some server error
+ *
+ */
+
+
+
+
+
 
 router.post('/register', Register.createUser)
 router.post('/login', Login.findUser)
