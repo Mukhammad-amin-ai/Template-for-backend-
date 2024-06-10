55a4e09b62e59bf6f406bff6f8d242b9b27c3da3
Mukhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2024-06-10 16:31:33 +0500

    fix

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ import mongoose from "mongoose";
 import router from "./routes/router.js";
 
 const app = Express();
-const PORT = 3000;
-// const DB = 'mongodb://localhost:27017/';
+const PORT = process.env.PORT || 3000;
+const DB = 'mongodb://localhost:27017/Steam';
 // const DB = "mongodb+srv://amin:user@cluster01.kcpynb7.mongodb.net/?retryWrites=true&w=majority"
-
 
 app.use(Express.json());
 app.use('/api', router);
@@ -15,11 +14,26 @@ const StarterFunc = async () => {
   try {
     await mongoose.connect(DB);
     app.listen(PORT, () => {
-      console.log('Server is working on port ' + PORT);
+      console.log(`\x1b[40m`,`\x1b[32m`,
+        `
+        ================================
+        
+       ___  __  ___  _______  _______
+      / _ \\/ / / / |/ /  _/ |/ / ___/
+     / , _/ /_/ /    // //    / (_ / 
+    /_/|_|\\____/_/|_/___/_/|_/\\___/  
+    
+    
+    =================================                            
+    [+] Maintance      : https://github.com/eminmuhammadi/emiga-stream.git
+    [+] Server         : http://localhost:${PORT}
+    [+] Socket         : ws: ++++
+    [~] Running Server...
+`,`\x1b[0m`);
     });
   } catch (e) {
     console.error(e);
   }
 };
 
-StarterFunc().then(r => console.log('Server is working on port ' + PORT));
+StarterFunc();
